770213a81b8b0f556cfb7fdd629c7a3d4e0f8913
Brian Park / 박기영 <125226023+BrianPark314@users.noreply.github.com>
2026-01-23 15:08:09 +0900

    Revert "feat: trailing comma 강제 규칙 추가 (autofix 지원)"

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "tmn",
-			Version: "0.3.0",
+			Version: "0.2.0",
 			Rules: []tflint.Rule{
 				rules.NewForEachTosetRule(),
 				rules.NewSortProviderAppMapRule(),
-				rules.NewRequireTrailingCommaRule(),
 			},
 		},
 	})
